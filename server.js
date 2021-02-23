04df9dfde931886aefe66f135a9c14a9f3a8cc38
Myles Crandall <you@example.com>
2021-02-23 10:15:08 -0500

    working csv to array

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { Deta } = require("deta");
 const app = express();
 const http = require('http').createServer(app)
 const multer  = require('multer')
+const fs = require('fs')
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads')
@@ -17,7 +18,8 @@ const storage = multer.diskStorage({
 })
 const upload = multer({storage})
 // application setup
-
+let dataobj = {};
+let data
 dotenv.config();
 
 const deta = Deta(process.env.PROJECT_KEY);
@@ -28,10 +30,41 @@ app.use(express.static("public"));
 
 
 app.post('/uploads',upload.single("Data"),(req,res)=>{
-    console.log(req.body);
+    
 })
-
+data = fs.readFileSync(__dirname + "/uploads/data.csv", 'utf8');
 // application routes
+
+//var csv is the CSV file with headers
+function csvJSON(csv){
+
+  var lines=csv.split("\n");
+
+  var result = [];
+
+  var headers=lines[0].split(",");
+
+  for(var i=1;i<lines.length;i++){
+
+      var obj = {};
+      var currentline=lines[i].split(",");
+
+      for(var j=0;j<headers.length;j++){
+          obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+
+  }
+  
+  //return result; //JavaScript object
+  return result //JSON
+}
+dataobj = csvJSON(data)
+
+    console.log(dataobj[1].Name)
+
+
 app.get("/", (request, response) => {
     response.sendFile(__dirname + "/Views/index.html")
 });
